d432a7c254991c99a73c106eb008d8e426121611
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-11-05 00:33:42 +0700

    fix: Image not verified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\HandleInertiaRequests;
+// use App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
+            $middleware->trustProxies(at: '*'),
             \Illuminate\Http\Middleware\HandleCors::class
         ]);
     })
