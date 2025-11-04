c7a81e6dcb781960aa1b12ffc7de227e0cce6dae
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-11-05 00:53:48 +0700

    fix: Image not verified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\HandleCors::class
